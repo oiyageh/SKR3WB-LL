9ce236720c648c53af15501cf8860086a18605ca
snley <snaileyc@gmail.com>
2026-04-27 23:56:14 -0400

    quick click script edit

--- Assets/Hailey's folder/HaileyScripts/ClickTrigger.cs
+++ Assets/Hailey's folder/HaileyScripts/ClickTrigger.cs
@@ -7,9 +7,6 @@ public class ClickDetector : MonoBehaviour
     public NPCData FirstNodeDayTwo;
     public NPCData FirstNodeDayThree;
     public NPCData FirstNodeDayFour;
-    public NPCData FirstNodeDayFive;
-    public NPCData FirstNodeDaySix;
-    public NPCData FirstNodeDaySeven;
 
     void OnMouseDown()
     {
@@ -34,21 +31,6 @@ public class ClickDetector : MonoBehaviour
         if (FirstNodeDayThree.nodeViewed == true && FirstNodeDayFour.nodeViewed == false)
         {
             GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<DialogueManager>().StartDialogue(FirstNodeDayFour);
-        }
-        //Day Five
-        if (FirstNodeDayFour.nodeViewed == true && FirstNodeDayFive.nodeViewed == false)
-        {
-            GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<DialogueManager>().StartDialogue(FirstNodeDayFive);
-        }
-        //Day Six
-        if (FirstNodeDayFive.nodeViewed == true && FirstNodeDaySix.nodeViewed == false)
-        {
-            GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<DialogueManager>().StartDialogue(FirstNodeDaySix);
-        }
-        //Day Seven
-        if (FirstNodeDaySix.nodeViewed == true && FirstNodeDaySeven.nodeViewed == false)
-        {
-            GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<DialogueManager>().StartDialogue(FirstNodeDaySeven);
         }
     }
 }
